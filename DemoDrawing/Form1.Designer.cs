3ad4523ddf01c2f4621af7709bc750705cf84a9d
Guillermo Calderon <guillermo.calderon@udb.edu.sv>
2021-01-27 14:08:49 -0600

    Agregar archivos de proyecto.

---
+++ DemoDrawing/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace DemoDrawing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbColores = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.nudY = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nudX = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudTamano = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbFigura = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTamano)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(628, 438);
+            this.panel1.TabIndex = 0;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbColores);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.nudY);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.nudX);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.nudTamano);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cmbFigura);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(635, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(194, 261);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Opciones";
+            // 
+            // cmbColores
+            // 
+            this.cmbColores.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbColores.FormattingEnabled = true;
+            this.cmbColores.Location = new System.Drawing.Point(57, 191);
+            this.cmbColores.Name = "cmbColores";
+            this.cmbColores.Size = new System.Drawing.Size(121, 21);
+            this.cmbColores.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Color:";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(52, 232);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(99, 23);
+            this.btnAgregar.TabIndex = 8;
+            this.btnAgregar.Text = "Agregar figura";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // nudY
+            // 
+            this.nudY.Location = new System.Drawing.Point(51, 151);
+            this.nudY.Maximum = new decimal(new int[] {
+            434,
+            0,
+            0,
+            0});
+            this.nudY.Name = "nudY";
+            this.nudY.Size = new System.Drawing.Size(62, 20);
+            this.nudY.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Y:";
+            // 
+            // nudX
+            // 
+            this.nudX.Location = new System.Drawing.Point(51, 112);
+            this.nudX.Maximum = new decimal(new int[] {
+            626,
+            0,
+            0,
+            0});
+            this.nudX.Name = "nudX";
+            this.nudX.Size = new System.Drawing.Size(62, 20);
+            this.nudX.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "X:";
+            // 
+            // nudTamano
+            // 
+            this.nudTamano.Location = new System.Drawing.Point(52, 67);
+            this.nudTamano.Name = "nudTamano";
+            this.nudTamano.Size = new System.Drawing.Size(120, 20);
+            this.nudTamano.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tamaño:";
+            // 
+            // cmbFigura
+            // 
+            this.cmbFigura.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFigura.FormattingEnabled = true;
+            this.cmbFigura.Items.AddRange(new object[] {
+            "Circulo",
+            "Cuadrado",
+            "Linea"});
+            this.cmbFigura.Location = new System.Drawing.Point(51, 22);
+            this.cmbFigura.Name = "cmbFigura";
+            this.cmbFigura.Size = new System.Drawing.Size(121, 21);
+            this.cmbFigura.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Figura:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Demo Drawing";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTamano)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.NumericUpDown nudY;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown nudX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudTamano;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbFigura;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbColores;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ DemoDrawing/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoDrawing
+{
+    public partial class Form1 : Form
+    {
+        Graphics area;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            area = panel1.CreateGraphics();
+            String[] colores = Enum.GetNames(typeof(System.Drawing.KnownColor));
+            cmbColores.Items.AddRange(colores);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            Point punto = panel1.PointToClient(Cursor.Position);
+            nudX.Value = punto.X;
+            nudY.Value = punto.Y;
+
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt16( nudX.Value);
+            int y = Convert.ToInt16(nudY.Value);
+            int size = Convert.ToInt16(nudTamano.Value);
+            int opcion = cmbFigura.SelectedIndex;
+            Color color = Color.FromName(cmbColores.SelectedItem.ToString());
+            Pen lapiz = new Pen(color, 5);
+            SolidBrush pincel = new SolidBrush(color);
+
+
+
+            switch (opcion)
+            {
+                case 0: // Circulo
+                    area.DrawEllipse(lapiz, x - size / 2, y - size / 2, size, size);
+                    area.FillEllipse(pincel, x - size / 2, y - size / 2, size, size);
+                    break;
+                case 1: // Cuadrado 
+                    area.DrawRectangle(lapiz, x, y, size, size);
+                    area.FillRectangle(pincel, x, y, size, size);
+                    break;
+                case 2: //linea
+                    area.DrawLine(lapiz, new Point(x, y), new Point(x + size, y));
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbColores.SelectedIndex = 0;
+            cmbFigura.SelectedIndex = 0;
+        }
+    }
+}
